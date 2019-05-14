9d854c59364647ce39e72b596f664cd8f7373e20
SHREYA GUPTA <sg055322@gmail.com>
2019-05-15 02:58:58 +0530

    Add files via upload

--- Github_assignment/scripts/app.js
+++ Github_assignment/scripts/app.js
@@ -1,3 +1,7 @@
+
+// const client_id = 'd9308aacf8b204d361fd';
+// const client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
+
 // Init UI
 const ui = new UI;
 
@@ -43,16 +47,7 @@ function getIssuesList(value,username,repo_name) {
 
 function fetchData(username,repo_name) {
   //API call to fetch open issues count via search query
-  var getOpenIssues = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.github.com/search/issues?q=repo:" + username + "/" + repo_name + "+is:public+is:issue+is:open");
-    xhr.onload = () => resolve(JSON.parse(xhr.responseText));
-    xhr.onerror = () => reject(xhr.statusText);
-    xhr.send();
-  });
-
-  //API call to fetch open issues count via repo query
-  var getTotalIssues = new Promise((resolve, reject) => {
+  var getOpenIssuesCount = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api.github.com/repos/" + username + "/" + repo_name);
     xhr.onload = () => resolve(JSON.parse(xhr.responseText));
@@ -60,14 +55,13 @@ function fetchData(username,repo_name) {
     xhr.send();
   });
 
-  Promise.all([getOpenIssues, getTotalIssues]).then(function(values) {//resoving both promises to get the results
-    let result1 = values[0];
-    let result2 = values[1];
-    let openIssues = result1.total_count;
-    let totalPages = Math.round(result2.open_issues_count/100);
+  Promise.all([getOpenIssuesCount]).then(function(values) {//resoving both promises to get the results
+    let open_issue_count = values[0].open_issues_count;
+    let totalPages = open_issue_count >100 ? Math.ceil(open_issue_count/100) : 1 ;
     let open24hrs = 0;
     let open7Days = 0;
     let opened = 0;
+    let openIssues = 0;
     let promises = [];
 
     for (let i = 1; i <= totalPages; i++) { 
@@ -77,24 +71,34 @@ function fetchData(username,repo_name) {
     Promise.all(promises).then((results) => {
       var merged = [].concat.apply([], results); //get one array for all resolved promisses data
       for(let i = 0; i < merged.length; i++){
-        let today = new Date(); // get current time
-        let creationDate = new Date(merged[i].created_at); // get issue created time
-        if (Math.abs(today.getTime() - creationDate.getTime()) / (1000 * 3600) <= 24) {  //Get open issues that were opened in the last 24 hours
-          open24hrs++;
-        } //Get open issues that were opened more than 24 hours ago but less than 7 days ago, 168 is the total no. of hours in a week. 
-        else if (Math.abs(today.getTime() - creationDate.getTime()) / (1000 * 3600) > 24 && Math.abs(today.getTime() - creationDate.getTime()) / (1000 * 3600) <= 168) {
-          open7Days++;
-        } else {
-          //Get open issues that were opened more than 7 days ago
-          opened++;
+        if(!merged[i].pull_request){
+          let today = new Date(); // get current time
+          let creationDate = new Date(merged[i].created_at); // get issue created time
+          if (Math.abs(today.getTime() - creationDate.getTime()) / (1000 * 3600) <= 24) {  //Get open issues that were opened in the last 24 hours
+            open24hrs++;
+          } //Get open issues that were opened more than 24 hours ago but less than 7 days ago, 168 is the total no. of hours in a week. 
+          else if (Math.abs(today.getTime() - creationDate.getTime()) / (1000 * 3600) > 24 && Math.abs(today.getTime() - creationDate.getTime()) / (1000 * 3600) <= 168) {
+            open7Days++;
+          } else {
+            //Get open issues that were opened more than 7 days ago
+            opened++;
+          }
         }
+        openIssues = open24hrs+open7Days+opened;
       }
       ui.showTable(openIssues, open24hrs, open7Days, opened);
+      // if error
+      if (results[0].message) {
+        ui.showAlert(values[0].message, 'alert alert-danger');
+        ui.clearTable();
+        searchUser.value = "";
+      }
     }).catch((error) => {
       console.log(error);
     });
-    if (result1.message) {
-      ui.showAlert(result1.message, 'alert alert-danger');
+    // if error
+    if (values[0].message) {
+      ui.showAlert(values[0].message, 'alert alert-danger');
       ui.clearTable();
       searchUser.value = "";
     }
